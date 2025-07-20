0089cf71cc481e5ad717564d2b8694d712187826
Maurice [Code045NL] <maurice@devtigspace.nl>
2025-07-20 14:24:19 +0200

    add modules class

--- templates/pages/settings-page.php
+++ templates/pages/settings-page.php
@@ -5,6 +5,7 @@ if (!defined('ABSPATH')) {
 
 // Get settings
 $settings = new WP_Git_Plugins_Settings('wp-git-plugins', '1.0.0');
+$modules_manager = new WP_Git_Plugins_Manager('wp-git-plugins', '1.0.0');
 ?>
 <div class="wrap">
     <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
@@ -17,14 +18,14 @@ $settings = new WP_Git_Plugins_Settings('wp-git-plugins', '1.0.0');
 <div id="post-body-content">
                 <div class="meta-box-sortables ui-sortable">
                     <div class="postbox">
-                        <h2 class="hndle"><span><?php esc_html_e('Debug Tools', 'wp-git-plugins'); ?></span></h2>
+                        <h2 class="hndle"><span><?php esc_html_e('Settings', 'wp-git-plugins'); ?></span></h2>
                         <div class="inside">
                             <h2 class="nav-tab-wrapper">
                                 <a href="#github-settings" class="nav-tab nav-tab-active">
-                                    <?php esc_html_e('History', 'wp-git-plugins'); ?>
+                                    <?php esc_html_e('Github Settings', 'wp-git-plugins'); ?>
                                 </a>
                                 <a href="#modules-manager" class="nav-tab">
-                                    <?php esc_html_e('Error Log', 'wp-git-plugins'); ?>
+                                    <?php esc_html_e('Modules Manager', 'wp-git-plugins'); ?>
                                 </a>
                             </h2>
                             
