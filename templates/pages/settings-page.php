271b80ff15e76c18932f983d9554209ef4385ef9
Maurice [Code045NL] <maurice@devtigspace.nl>
2025-07-20 15:11:35 +0200

    settings page

--- templates/pages/settings-page.php
+++ templates/pages/settings-page.php
@@ -3,44 +3,57 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
+/**
+ * Settings Page Template for WP Git Plugins
+ *
+ * This file is part of the WP Git Plugins project.
+ *
+ * @package WP_Git_Plugins
+ * @since 1.0.0
+ */
+
 // Get settings
 $settings = new WP_Git_Plugins_Settings('wp-git-plugins', '1.0.0');
 ?>
-<div class="wrap">
-    <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+<div class="wrap wp-git-plugins-settings">
+    <h1><?php esc_html_e('WP Git Plugins - Settings', 'wp-git-plugins'); ?></h1>
     
-    <?php settings_errors('wpgp_messages'); ?>
-    
-    <div class="wp-git-plugins-container">
-        <div class="wp-git-plugins-main">
-            <!-- Tab Navigation -->
-<div id="post-body-content">
+    <div id="poststuff">
+        <div id="post-body" class="metabox-holder columns-2">
+            <!-- Main content -->
+            <div id="post-body-content">
                 <div class="meta-box-sortables ui-sortable">
                     <div class="postbox">
                         <h2 class="hndle"><span><?php esc_html_e('Settings', 'wp-git-plugins'); ?></span></h2>
                         <div class="inside">
                             <h2 class="nav-tab-wrapper">
-                                <a href="#github-settings" class="nav-tab nav-tab-active">
+                                <a href="#git-settings" class="nav-tab nav-tab-active">
                                     <?php esc_html_e('Github Settings', 'wp-git-plugins'); ?>
                                 </a>
-                                <a href="#modules-manager" class="nav-tab">
-                                    <?php esc_html_e('Modules Manager', 'wp-git-plugins'); ?>
+                                <a href="#model-settings" class="nav-tab">
+                                    <?php esc_html_e('Model Settings', 'wp-git-plugins'); ?>
                                 </a>
                             </h2>
                             
                             <div class="tab-content-wrapper">
                                 <!-- Github Settings Tab -->
-                                <div id="github-settings" class="tab-content active">
+                                <div id="git-settings" class="tab-content active">
                                     <?php include WP_GIT_PLUGINS_DIR . 'templates/components/settings/github-settings.php'; ?>
                                 </div>
                                 
-                                <!-- Modules Manager Tab -->
-                                <div id="modules-manager" class="tab-content">
+                                <!-- Model Settings Tab -->
+                                <div id="model-settings" class="tab-content">
                                     <?php include WP_GIT_PLUGINS_DIR . 'templates/components/settings/module-settings.php'; ?>
                                 </div>
-                                
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            
+            <!-- Sidebar -->
+            <div class="wp-git-plugins-sidebar">
+            <?php include WP_GIT_PLUGINS_DIR . 'templates/components/plugin-info.php'; ?>
+        </div>   
+     </div>
+</div>
