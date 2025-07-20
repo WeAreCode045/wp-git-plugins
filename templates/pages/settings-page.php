b08741c9d8948ae416d8410d29a8e932d44e7399
Maurice [Code045NL] <maurice@devtigspace.nl>
2025-07-20 14:15:54 +0200

    settings tab fix

--- templates/pages/settings-page.php
+++ templates/pages/settings-page.php
@@ -14,79 +14,33 @@ $settings = new WP_Git_Plugins_Settings('wp-git-plugins', '1.0.0');
     <div class="wp-git-plugins-container">
         <div class="wp-git-plugins-main">
             <!-- Tab Navigation -->
-            <nav class="nav-tab-wrapper wp-clearfix">
-                <a href="#github-settings" class="nav-tab nav-tab-active" id="github-settings-tab">
-                    <span class="dashicons dashicons-admin-generic"></span>
-                    <?php esc_html_e('GitHub Settings', 'wp-git-plugins'); ?>
-                </a>
-                <a href="#modules-management" class="nav-tab" id="modules-management-tab">
-                    <span class="dashicons dashicons-admin-plugins"></span>
-                    <?php esc_html_e('Module Management', 'wp-git-plugins'); ?>
-                </a>
-            </nav>
-            
-            <!-- Tab Content -->
-            <div class="tab-content-wrapper">
-                <!-- GitHub Settings Tab -->
-                <div id="github-settings" class="tab-content active">
-                    <?php include WP_GIT_PLUGINS_DIR . 'templates/components/settings/github-settings.php'; ?>
-                </div>
-                
-                <!-- Modules Management Tab -->
-                <div id="modules-management" class="tab-content">
-                    <?php include WP_GIT_PLUGINS_DIR . 'templates/components/settings/module-settings.php'; ?>
+<div id="post-body-content">
+                <div class="meta-box-sortables ui-sortable">
+                    <div class="postbox">
+                        <h2 class="hndle"><span><?php esc_html_e('Debug Tools', 'wp-git-plugins'); ?></span></h2>
+                        <div class="inside">
+                            <h2 class="nav-tab-wrapper">
+                                <a href="#github-settings" class="nav-tab nav-tab-active">
+                                    <?php esc_html_e('History', 'wp-git-plugins'); ?>
+                                </a>
+                                <a href="#modules-manager" class="nav-tab">
+                                    <?php esc_html_e('Error Log', 'wp-git-plugins'); ?>
+                                </a>
+                            </h2>
+                            
+                            <div class="tab-content-wrapper">
+                                <!-- Github Settings Tab -->
+                                <div id="github-settings" class="tab-content active">
+                                    <?php include WP_GIT_PLUGINS_DIR . 'templates/components/settings/github-settings.php'; ?>
+                                </div>
+                                
+                                <!-- Modules Manager Tab -->
+                                <div id="modules-manager" class="tab-content">
+                                    <?php include WP_GIT_PLUGINS_DIR . 'templates/components/settings/module-settings.php'; ?>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        
-        <div class="wp-git-plugins-sidebar">
-            <?php include WP_GIT_PLUGINS_DIR . 'templates/components/plugin-info.php'; ?>
-        </div>
-    </div>
-</div>
-
-<script>
-console.log('Settings page loaded');
-jQuery(document).ready(function($) {
-    console.log('Settings page jQuery ready');
-    console.log('Found tabs:', $('.nav-tab').length);
-    console.log('Found tab content:', $('.tab-content').length);
-    
-    // Ensure initial state is correct
-    $('.tab-content').hide();
-    $('.tab-content.active').show();
-    
-    // Test direct tab click handler
-    $('.nav-tab').on('click', function(e) {
-        console.log('Tab clicked:', $(this).attr('href'));
-        e.preventDefault();
-        
-        var target = $(this).attr('href');
-        
-        // Remove active class from all tabs
-        $('.nav-tab').removeClass('nav-tab-active');
-        
-        // Add active class to clicked tab
-        $(this).addClass('nav-tab-active');
-        
-        // Hide all tab content
-        $('.tab-content').removeClass('active').hide();
-        
-        // Show target tab content
-        $(target).addClass('active').show();
-        
-        console.log('Tab switched to:', target);
-    });
-});
-</script>
-
-<style>
-/* Ensure tab content is properly controlled */
-.tab-content {
-    display: none !important;
-}
-
-.tab-content.active {
-    display: block !important;
-}
-</style>
